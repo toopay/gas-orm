e8726804dd36ba7e872445c7516d4449359d1fb3
toopay <toopay@taufanaditya.com>
2011-11-03 18:18:36 +0700

    Italian language from Stefano (www.stefanogiordano.it)

---
+++ language/italian/gas_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['models_not_found']          = 'Il percorso %s specificato nel file di configurazione non &egrave; valido.';
+
+$lang['models_found_no_relations'] = 'Nessuna relazione valida trovata nel Model %s.';
+
+$lang['empty_arguments']           = '%s non pu&ograve; essere invocato senza parametri.';
+
+
+$lang['auto_check']                = 'Il campo %s non &egrave; un campo di tipo autoincrement valido.';
+
+$lang['char_check']                = 'Il campo %s non &egrave; un campo di tipo char valido.';
