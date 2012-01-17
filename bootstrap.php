46cb5f9c49e61f9dae96d041543bb96161f11801
Taufan Aditya <toopay@presario-natty.(none)>
2012-01-18 01:09:32 +0700

    Fix Travis bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -10,13 +10,14 @@
 define('ENVIRONMENT', 'testing');
 define('APPPATH',  __DIR__.DIRECTORY_SEPARATOR);
 define('BASEPATH', 'vendor'.DIRECTORY_SEPARATOR.'CodeIgniter'.DIRECTORY_SEPARATOR.'system'.DIRECTORY_SEPARATOR);
+define('SRCPATH',  APPPATH.'third_party'.DIRECTORY_SEPARATOR.'gas'.DIRECTORY_SEPARATOR);
 
 // Define Gas ORM configuration for unit testing
-$config = array('models_path'        => array('Model' => __DIR__.DIRECTORY_SEPARATOR.'dummyModels'),
+$config = array('models_path'        => array('Model' => SRCPATH.'tests'.DIRECTORY_SEPARATOR.'dummyModels'),
                 'cache_request'      => TRUE,
                 'auto_create_models' => FALSE,
                 'auto_create_tables' => FALSE);
 
 
 // Include Gas ORM configuration and Bootstrap
-include_once APPPATH.'third_party'.DIRECTORY_SEPARATOR.'gas'.DIRECTORY_SEPARATOR.'bootstrap.php';
+include_once SRCPATH.'bootstrap.php';
