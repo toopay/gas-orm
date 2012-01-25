3039ede417045f31db92b2e112d581a5f992f31d
Taufan Aditya <toopay@taufanaditya.com>
2012-01-25 20:34:08 +0700

    DBAL test

---
+++ third_party/gas/tests/testCases/DBALTests/ConnectionTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Gas ORM Unit Test
+ *
+ * Test case for connection method
+ *
+ * @package     Gas ORM
+ * @category    Unit Test
+ * @version     2.0.0
+ * @author      Taufan Aditya
+ */
+
+class ConnectionTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @see Gas\Core (./classes/core.php)
+     */
+    public function tearDown()
+    {
+        // Re-connect using related environment
+        Gas\Core::connect(DB_GROUP);
+    }
+
+    /**
+     * @see Gas\Core (./classes/core.php)
+     */
+    public function testConnectionViaGroup()
+    {
+        // Connect using a valid group name in config
+        Gas\Core::connect('testing_mysql');
+
+        $db = Gas\Core::$db;
+
+        $this->assertInstanceOf('CI_DB', $db);
+        $this->assertInstanceOf('CI_DB_Driver', $db);
+    }
+
+    /**
+     * @see Gas\Core (./classes/core.php)
+     */
+    public function testConnectionViaDsnPDODriver()
+    {
+        // Connect using a valid dsn string
+        Gas\Core::connect('pdo://travis:@localhost:3306/gas_test?provider=mysql');
+
+        $db = Gas\Core::$db;
+
+        $this->assertInstanceOf('CI_DB', $db);
+        $this->assertInstanceOf('CI_DB_Driver', $db);
+        $this->assertInstanceOf('CI_DB_PDO_Driver', $db);
+    }
+
+    /**
+     * @see Gas\Core (./classes/core.php)
+     */
+    public function testConnectionViaDsnNativeDriver()
+    {
+        // Connect using a valid dsn string
+        Gas\Core::connect('mysql://travis:@localhost:3306/gas_test');
+
+        $db = Gas\Core::$db;
+
+        $this->assertInstanceOf('CI_DB', $db);
+        $this->assertInstanceOf('CI_DB_Driver', $db);
+        $this->assertInstanceOf('CI_DB_MySQL_Driver', $db);
+    }
+}
---
+++ third_party/gas/tests/testCases/DBALTests/QueryTest.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Gas ORM Unit Test
+ *
+ * Test case for query method
+ *
+ * @package     Gas ORM
+ * @category    Unit Test
+ * @version     2.0.0
+ * @author      Taufan Aditya
+ */
+
+class QueryTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @var object Database holder
+     */
+    public $db;
+
+    /**
+     * @see Gas\Core   (./classes/core.php)
+     * @see Model\User (./tests/dummyModels/user.php)
+     * @see Model\Wife (./tests/dummyModels/wife.php)
+     */
+    public function setUp()
+    {
+        // Prepare some table(s) for test,
+        // and connect using related environment
+        Model\User::setUp();
+        Model\Wife::setUp();
+        $this->db = Gas\Core::$db;
+    }
+
+    /**
+     * @see Model\User (./tests/dummyModels/user.php)
+     */
+    public function testSimpleQuery()
+    {
+        // Execute some simple query
+        // this should return FALSE, 
+        // since `foo` table was not exists
+        $sql    = $this->db->_prep_query('SELECT * FROM `foo`');
+        $result = $this->db->simple_query($sql);
+        $this->assertFalse($result);
+
+        // Execute some simple query
+        // this should return a PDOStatement instance, 
+        // since `user` table was exists
+        $sql    = $this->db->_prep_query('SELECT * FROM `user`');
+        $result = $this->db->simple_query($sql);
+        $this->assertInstanceOf('PDOStatement', $result);
+
+        // This also mean we could use some PDO fetch method directly on it
+        $first_row = $result->fetch(PDO::FETCH_ASSOC);
+
+        // Check the result
+        $this->assertArrayHasKey('id', $first_row);
+        $this->assertArrayHasKey('name', $first_row);
+        $this->assertArrayHasKey('username', $first_row);
+        $this->assertArrayHasKey('email', $first_row);
+    }
+
+    /**
+     * @see Model\Wife (./tests/dummyModels/wife.php)
+     */
+    public function testQuery()
+    {
+        // Execute some query this should return FALSE,
+        // since `foo` table was not exists
+        // Note that unlike with simple query, 
+        // we do not need to escape it will automatically prepared and escaped
+        $sql    = 'SELECT * FROM `foo`';
+        $result = $this->db->query($sql);
+        $this->assertFalse($result);
+
+        // this should return a CI_DB_Result instance, 
+        // and in this particular test it would be a CI_DB_pdo_Result instance, 
+        // since `wife` table was exists
+        $sql    = 'SELECT * FROM `wife`';
+        $result = $this->db->query($sql);
+        $this->assertInstanceOf('CI_DB_result', $result);
+        $this->assertInstanceOf('CI_DB_pdo_result', $result);
+
+        // A result should can calling `result`, `result_array` and `result_object`
+        $this->assertEquals(is_callable(array($result, 'result'), TRUE), TRUE);
+        $this->assertEquals(is_callable(array($result, 'result_array'), TRUE), TRUE);
+        $this->assertEquals(is_callable(array($result, 'result_object'), TRUE), TRUE);
+
+        // Check the meta-information about this query
+        $this->assertEquals($result->num_rows(), 3);
+        $this->assertEquals($result->num_fields(), 4);
+    }
+}
