e9305661e91c66fb40bb194f5445368609fd9060
Taufan Aditya <toopay@taufanaditya.com>
2013-02-13 20:28:17 +0700

    Covering Janitor

--- third_party/gas/tests/testCases/Coverage/JanitorTest.php
+++ third_party/gas/tests/testCases/Coverage/JanitorTest.php
@@ -13,8 +13,33 @@
 
 class JanitorTest extends PHPUnit_Framework_TestCase {
 
-	public function testGetInputThrowExceptionIfEmptyInput()
+	public function testGetGlobalPath()
 	{
+		// Test basepath
+		$basePath = Gas\Janitor::path('base');
+
+		$this->assertTrue(is_dir($basePath));
+
+		// Test apppath
+		$appPath = Gas\Janitor::path('app');
+
+		$this->assertTrue(is_dir($appPath));
+
+		// Test invalid path
+		$this->setExpectedException('InvalidArgumentException', 'empty_arguments:path');
+		$undefinedPath = Gas\Janitor::path('undefined');
+	}
+
+	public function testGetInput()
+	{
+		$sanity_input = Gas\Janitor::get_input('some_method', array('foo'=>'bar'), TRUE);
+
+		$this->assertArrayHasKey('foo',$sanity_input);
+
+		$continue_even_empty = Gas\Janitor::get_input('some_method', array(), FALSE);
+
+		$this->assertEmpty($continue_even_empty);
+
 		$this->setExpectedException('InvalidArgumentException', 'empty_arguments:some_method');
 		
 		Gas\Janitor::get_input('some_method', NULL, TRUE);
