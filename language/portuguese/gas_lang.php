b316e3aa8dcb128849165316cd81fd6e99b9d76a
Taufan Aditya <toopay@taufanaditya.com>
2013-02-13 22:32:41 +0700

    Portuguese lang

---
+++ language/portuguese/gas_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['db_connection_error']       = 'N&atilde;o foi possivel connectar &agrave; base de dados com a sua configuraç&atilde;o / DNS : %s';
+$lang['models_not_found']          = 'N&atilde;o foi possivel localizar o(s) model(s) que especificou: %s';
+$lang['models_found_no_relations'] = 'O Model %s foi encontrado, mas n&atilde;o contém informaç&atilde;o de relacionamento.';
+$lang['extensions_not_found']      = 'N&atilde;o foi possivel localizar a extenss&atilde;o com o caminho que especificou: %s';
+$lang['empty_arguments']           = 'N&atilde;o pode continuar o processo %s se n&atilde;o forem passados os parametros necess&aacute;rios.';
+$lang['cannot_create_model']       = 'Gas ORM n&atilde;o encontrou o model(s): %s';
+$lang['cannot_create_migration']   = 'Gas ORM n&atilde;o criou o(s) ficheiro(s) de migraç&atilde;o: %s';
+$lang['migration_no_setting']      = 'Gas ORM auto-migrate foi parado, n&atilde;o foi possivel encontrar as configuraç&atilde;es.';
+$lang['migration_no_dir']          = 'Gas ORM auto-migrate foi parado, n&atilde;o foi encontrado um directorio v&aacute;lido.';
+$lang['migration_no_initial']      = 'Gas ORM auto-migrate foi parado porque a vers&atilde;o da migraç&atilde;o because est&aacute; acima de \'0\'.';
+$lang['migration_disabled']        = 'Gas ORM auto-migrate foi parado porque a biblioteca de migraç&atilde;o n&atilde;o est&aacute; activa.';
+$lang['both_auto_error']           = 'Gas ORM cannot executou tanto as tabelas como models,'
+                                    .' mas desactivou um deles.';
+
+$lang['auto_check']                = 'O %s campo n&atilde;o &eacute; v&aacute;lido como auto-incremento.';
+$lang['char_check']                = 'O %s campo n&atilde;o &acute; v&aacute;lido como caracters.';
+$lang['date_check']                = 'O %s campo n&atilde;o cont&eacute;m um datetime v&aacute;lido.';
