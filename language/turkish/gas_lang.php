62ad6bd6eccf2bdd3557e5d14e55cb7a819debdf
Serkan Algur <info@wpadami.com>
2015-08-07 11:19:34 +0300

    Turkish Language
    The Turkish Language of GAS-ORM

---
+++ language/turkish/gas_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['db_connection_error']       = 'Bu bağlantı grubuyla da ya da dns bilgisiyle veritabanına bağlanamazsınız : %s';
+$lang['models_not_found']          = 'Belirttiğiniz model dosyalarına erişemiyoruz: %s';
+$lang['models_found_no_relations'] = '%s modeli bulundu ama ilişki özellikleri kayıp.';
+$lang['extensions_not_found']      = 'Belirttiğiniz eklenti alanı bulunamadı: %s';
+$lang['empty_arguments']           = 'Gönderilmiş parametreler olmadan %s işlemine devam edemiyoruz.';
+$lang['cannot_create_model']       = 'Gas ORM model dosyası oluşturamadu: %s';
+$lang['cannot_create_migration']   = 'Gas ORM taşıma dosyası oluşturamadı: %s';
+$lang['migration_no_setting']      = 'Gas ORM otomatik taşıma gerçekleşemedi çünkü seçenekler bulunamadı.';
+$lang['migration_no_dir']          = 'Gas ORM otomatik taşıma gerçekleşemedi çünkü belirtilen dizin bulunamadı.';
+$lang['migration_no_initial']      = 'Gas ORM otomatik taşıma gerçekleşemedi çünkü versiyon \'0\'.';
+$lang['migration_disabled']        = 'Gas ORM otomatik taşıma gerçekleşemedi çünkü taşıma kütüphanesi devredışı.';
+$lang['both_auto_error']           = 'Gas ORM otomatik oluşlturulmuş tablolarla modelleri aynı anda kullanamaz,'
+                                    .' birini devre dışı bırakın.';
+
+$lang['auto_check']                = '%s sütunu geçersiz bir otomatik artış sütunu.';
+$lang['char_check']                = '%s sütunu geçersiz bir karakter sütunu.';
+$lang['date_check']                = '%s sütunu geçersiz bir tarih sütunu.';
